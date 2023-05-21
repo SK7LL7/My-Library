52a54cb7b5647fa30b13a25708e8373e622f6988
SK7LL <126547381+SK7LL7@users.noreply.github.com>
2023-05-21 09:47:24 +0800

    Add files via upload

---
+++ Health var/health.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+var maxHealth: int = 100
+var currentHealth: int = 100
+
+func _ready():
+    pass
+
+func takeDamage(damage: int):
+    currentHealth -= damage
+    if currentHealth <= 0:
+        currentHealth = 0
+        onHealthDepleted()
+    else:
+        onHealthChanged()
+
+func heal(healingAmount: int):
+    currentHealth += healingAmount
+    if currentHealth > maxHealth:
+        currentHealth = maxHealth
+    onHealthChanged()
+
+func onHealthChanged():
+    # Implement logic to update UI or perform actions when health changes
+    print("Current Health: ", currentHealth)
+
+func onHealthDepleted():
+    # Implement logic when health reaches 0
+    print("Health Depleted!")
+
+# Usage:
+# Create an instance of this script in your scene and call `takeDamage` and `heal` functions as needed.
+# Example usage:
+# `takeDamage(20)` to reduce health by 20.
+# `heal(30)` to increase health by 30.
